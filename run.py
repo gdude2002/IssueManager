773f5982ee740778ce403c9c4e4cc8eca6bbe6fb
Gareth Coles <gdude2002@gmail.com>
2014-11-10 13:33:58 +0000

    Initial commit

---
+++ run.py
@@ -0,0 +1,8 @@
+"""
+This is a bot designed to work with GitHub issues for certain repos.
+
+This was designed for use with the GlowstoneMC project, but it may be generic
+enough for you to use with your own projects as well.
+"""
+
+__author__ = 'Gareth Coles'
---
+++ system/manager.py
@@ -0,0 +1,8 @@
+__author__ = 'Gareth Coles'
+
+import logging
+import yaml
+
+
+class Manager(object):
+    pass
